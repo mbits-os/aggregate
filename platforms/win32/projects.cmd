151a486f5e913cc3a83fb0ff572510a9d5aedaac
Marcin Zdun <mzdun@midnightbits.com>
2013-03-18 20:19:21 +0100

    Changing library name

--- platforms/win32/projects.cmd
+++ platforms/win32/projects.cmd
@@ -1,4 +1,4 @@
 @create_projects.py ../../ 3rdparty StaticLibrary BD334C61-C2F4-4D62-B08C-DBB562F0BA43
-@create_projects.py ../../ dbconn StaticLibrary 78037AD4-D33B-4641-A9F7-D0A5E142B487
+@create_projects.py ../../ libenv StaticLibrary 78037AD4-D33B-4641-A9F7-D0A5E142B487
 @create_projects.py ../../ server Application 93A32A2E-A7A6-4ABF-9505-BB6A76A2F358
 @create_projects.py ../../ dbtool Application 1B38F6B4-1D10-4076-AB73-0F8B5B652ED8
