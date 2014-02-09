1a9a98e36d9881985e6cf3c025e89f0e5f49f354
Marcin Zdun <mzdun@midnightbits.com>
2014-02-09 23:13:19 +0100

    Adding missing project generation

--- platforms/win32/projects.cmd
+++ platforms/win32/projects.cmd
@@ -2,3 +2,4 @@
 @create_projects.py ../../ libenv StaticLibrary 78037AD4-D33B-4641-A9F7-D0A5E142B487
 @create_projects.py ../../ server Application 93A32A2E-A7A6-4ABF-9505-BB6A76A2F358
 @create_projects.py ../../ dbtool Application 1B38F6B4-1D10-4076-AB73-0F8B5B652ED8
+@create_projects.py ../../ spawn-fcgi Application 0C5C2102-0739-4C8B-96DD-8BB9ED6E189B
