4402e22f4ed159012edca65c4bad80f81f8e2a60
Marcin Zdun <mzdun@midnightbits.com>
2014-02-15 17:27:02 +0100

    Last fix for spawn-fcgi removal

--- platforms/win32/projects.cmd
+++ platforms/win32/projects.cmd
@@ -3,4 +3,3 @@
 @create_projects.py ../../ libremote StaticLibrary 76C6A49A-2917-49D0-9AC5-DFBF5C3983E0
 @create_projects.py ../../ server Application 93A32A2E-A7A6-4ABF-9505-BB6A76A2F358
 @create_projects.py ../../ dbtool Application 1B38F6B4-1D10-4076-AB73-0F8B5B652ED8
-@create_projects.py ../../ spawn-fcgi Application 0C5C2102-0739-4C8B-96DD-8BB9ED6E189B
