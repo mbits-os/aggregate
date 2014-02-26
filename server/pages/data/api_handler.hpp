a7dd4a7404703820ffd5d29f86f59236c5c62955
Marcin Zdun <mzdun@midnightbits.com>
2014-02-26 12:02:46 +0100

    Small fixes for __FILE__ filtering

--- server/pages/data/api_handler.hpp
+++ server/pages/data/api_handler.hpp
@@ -71,8 +71,8 @@ namespace FastCGI { namespace app { namespace api
 #if DEBUG_CGI
 			APIOperationDbgInfo info;
 			info.ptr = ptr;
-#ifdef POSIX
-			info.file = file + 6; // skip "../../"
+#if defined(POSIX) || defined(NDEBUG)
+			info.file = file + 6; // skip "../../" (or "..\..\")
 #else
 			info.file = file + BUILD_DIR_LEN;
 #endif
