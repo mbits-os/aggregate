701afd3b0a0b5525d0db35175f79226d2cbc64f0
Marcin Zdun <mzdun@midnightbits.com>
2014-03-08 15:30:43 +0100

    Handler for 400 and 404

--- server/error_pages.cpp
+++ server/error_pages.cpp
@@ -30,29 +30,55 @@
 
 namespace FastCGI { namespace app { namespace reader { namespace errors {
 
+        struct String
+        {
+                lng::LNG lngId;
+                const char* fallback;
+        };
+
+	struct FallbackTranslation
+	{
+		PageTranslation tr;
+		bool hasStrings;
+	public:
+		void init(const SessionPtr& session, Request& request)
+		{
+			hasStrings = tr.init(session, request);
+		}
+
+		const char* operator()(const String& s)
+		{
+			return hasStrings ? tr(s.lngId) : s.fallback;
+		}
+
+		const char* operator()(lng::LNG id, const char* fallback)
+		{
+                        return hasStrings ? tr(id) : fallback;
+		}
+	};
+
 	class ErrorPageHandler : public ErrorHandler
 	{
 	protected:
-		virtual const char* pageTitle(int errorCode) { return nullptr; }
-		virtual const char* pageTitle(int errorCode, PageTranslation& tr) { return pageTitle(errorCode); }
-		std::string getTitle(int errorCode, Request& request, PageTranslation& tr, bool hasStrings)
+		virtual const char* pageTitle(int errorCode, FallbackTranslation& tr) { return nullptr; }
+		std::string getTitle(int errorCode, Request& request, FallbackTranslation& tr)
 		{
-			std::string title = hasStrings ? tr(lng::LNG_GLOBAL_SITENAME) : "reedr";
-			const char* page = hasStrings ? pageTitle(errorCode, tr) : pageTitle(errorCode);
+			std::string title = tr(lng::LNG_GLOBAL_SITENAME, "reedr");
+			const char* page = pageTitle(errorCode, tr);
 			if (!page) return title;
 			title += " &raquo; ";
 			title += page;
 			return title;
 		}
 
-		virtual void header(int errorCode, const SessionPtr& session, Request& request, PageTranslation& tr, bool hasStrings)
+		virtual void header(int errorCode, const SessionPtr& session, Request& request, FallbackTranslation& tr)
 		{
 			request << "<!DOCTYPE html "
 				"PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" "
 				"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\r\n"
 				"<html>\r\n"
 				"  <head>\r\n"
-				"    <title>" << getTitle(errorCode, request, tr, hasStrings) << "</title>\r\n"
+				"    <title>" << getTitle(errorCode, request, tr) << "</title>\r\n"
 				"    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>\r\n"
 				"    <style type=\"text/css\">@import url(\"" << static_web << "css/site.css\");</style>\r\n"
 				"    <style type=\"text/css\">@import url(\"" << static_web << "css/topbar.css\");</style>\r\n"
@@ -64,7 +90,7 @@ namespace FastCGI { namespace app { namespace reader { namespace errors {
 			request <<
 				"    <style type=\"text/css\">@import url(\"" << static_web << "css/fd_icons.css\");</style>\r\n";
 #endif
-			const char* description = hasStrings ? tr(lng::LNG_GLOBAL_DESCRIPTION) : "stay up to date";
+			const char* description = tr(lng::LNG_GLOBAL_DESCRIPTION, "stay up to date");
 			request <<
 				"    <style type=\"text/css\">@import url(\"" << static_web << "css/site-logo-big.css\");</style>\r\n"
 				"    <style type=\"text/css\">@import url(\"" << static_web << "css/tabs.css\");</style>\r\n"
@@ -83,7 +109,7 @@ namespace FastCGI { namespace app { namespace reader { namespace errors {
 				"  <div id=\"form-content\">\r\n";
 		}
 
-		virtual void footer(const SessionPtr& session, Request& request, PageTranslation& tr, bool hasStrings)
+		virtual void footer(const SessionPtr& session, Request& request, FallbackTranslation& tr)
 		{
 			request << "\r\n"
 				"  </div> <!-- form-content -->\r\n"
@@ -91,7 +117,7 @@ namespace FastCGI { namespace app { namespace reader { namespace errors {
 				"</html\r\n";
 		}
 
-		virtual void contents(int errorCode, const SessionPtr& session, Request& request, PageTranslation& tr, bool hasStrings)
+		virtual void contents(int errorCode, const SessionPtr& session, Request& request, FallbackTranslation& tr)
 		{
 			request <<
 				"    <h1>" << errorCode << "</h1>\r\n"
@@ -102,19 +128,61 @@ namespace FastCGI { namespace app { namespace reader { namespace errors {
 		void onError(int errorCode, Request& request) override
 		{
 			auto session = request.getSession(false);
-			PageTranslation tr{};
+			FallbackTranslation tr{};
 
-			bool hasStrings = tr.init(session, request);
+			tr.init(session, request);
 
-			header(errorCode, session, request, tr, hasStrings);
-			contents(errorCode, session, request, tr, hasStrings);
-			footer(session, request, tr, hasStrings);
+			header(errorCode, session, request, tr);
+			contents(errorCode, session, request, tr);
+			footer(session, request, tr);
+		}
+	};
+
+	struct ErrorInfo
+	{
+		String title;
+		String oops;
+		String info;
+	};
+
+	static inline void error_contents(Request& request, FallbackTranslation& tr, const ErrorInfo& nfo)
+	{
+		request << "<h1>" << tr(nfo.title) << "</h1>\r\n"
+			"<p class='oops'>" << tr(nfo.oops) << "</p>\r\n"
+			"<p>" << tr(nfo.info) << "</p>\r\n";
+	}
+
+	class Error404 : public ErrorPageHandler
+	{
+	public:
+		void contents(int errorCode, const SessionPtr& session, Request& request, FallbackTranslation& tr) override
+		{
+			error_contents(request, tr, {
+				{ lng::LNG_ERROR_404_TITLE, "404<br/>FILE NOT FOUND" },
+				{ lng::LNG_ERROR_404_OOPS,  "Well, that was not supposed to happen." },
+				{ lng::LNG_ERROR_404_INFO,  "This link has nothing to show, maybe you mistyped the link or followed a bad one." }
+			});
+		}
+	};
+
+	class Error400 : public ErrorPageHandler
+	{
+	public:
+		void contents(int errorCode, const SessionPtr& session, Request& request, FallbackTranslation& tr) override
+		{
+			error_contents(request, tr, {
+				{ lng::LNG_ERROR_400_TITLE, "400<br/>BAD REQUEST" },
+				{ lng::LNG_ERROR_400_OOPS,  "Hmm, this request looks mighty strange." },
+				{ lng::LNG_ERROR_400_INFO,  "The request your browser made was not understood." }
+			});
 		}
 	};
 
 	void setErrorHandlers(Application& app)
 	{
 		app.setErrorHandler(0, std::make_shared<ErrorPageHandler>());
+		app.setErrorHandler(400, std::make_shared<Error400>());
+		app.setErrorHandler(404, std::make_shared<Error404>());
 	}
 
 }}}} // FastCGI::app::reader::errors
