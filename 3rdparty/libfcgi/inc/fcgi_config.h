0c19fe9a0328f5663eeb2b7acb5fa42243f97f4c
Marcin Zdun <mzdun@midnightbits.com>
2014-03-13 15:37:06 +0100

    Deleting the 3rdparty code

--- 3rdparty/libfcgi/inc/fcgi_config.h
+++
@@ -1,9 +0,0 @@
-/* 
- *  Copied to fcgi_config.h when building on WinNT without cygwin,
- *  i.e. configure is not run.  See fcgi_config.h.in for details.
- */
-#ifdef _WIN32
-#include "fcgi_config_win32.h"
-#else
-#include "fcgi_config_posix.h"
-#endif
